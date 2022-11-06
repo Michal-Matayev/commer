9630d5445b66d223ae5ceca7cd6541f718c5e3cd
Michal Matayev <mmichal2204@gmail.com>
2022-11-06 11:53:23 +0200

    recursion option

--- tree.js
+++ tree.js
@@ -1,3 +1,25 @@
+// "use strict";
+// let myRequest = new Request("./data.json");
+
+// fetch(myRequest)
+//     .then(function (resp) {
+//         return resp.json();
+//     })
+//     .then(function (data) {
+//         console.log(data);
+//         for (let i = 0; i < data.length; i++) {
+//             document.getElementById("id").innerHTML = data[i].id;
+//             document.getElementById("name").innerHTML = data[i].name;
+//             document.getElementById("url").innerHTML = data[i].url;
+
+
+//         }
+            
+//     })
+
+
+
+
 "use strict";
 let myRequest = new Request("./data.json");
 
@@ -7,14 +29,45 @@ fetch(myRequest)
     })
     .then(function (data) { 
         console.log(data);
-        for (let i = 0; i < data.length; i++) {
-            document.getElementById("id").innerHTML = data[i].id;
-            document.getElementById("name").innerHTML = data[i].name;
-            document.getElementById("url").innerHTML = data[i].url;
+
+        data.filter(item => {
+            iteratorObject(item);
+        });
+
+        function iteratorObject(obj) { 
+            for (let prop in obj) { 
+                if (typeof (obj[prop]) == "object") {
+                    iteratorObject(obj[prop]);
+                }
+                else { 
+                    console.log(prop.toUpperCase() + ":", obj[prop]);
+                    if (prop == "id" ) { 
+                        document.getElementById("id").innerHTML =  obj[prop];
+                    }
+                    if (prop == "name") {
+                        document.getElementById("name").innerHTML =  obj[prop];
+                    }
+                    if (prop == "url") { 
+                        document.getElementById("url").innerHTML =  obj[prop];
+                    }
+
+                }
+
+            }
 
         }
+
+
+        
             
     })
 
 
 
+
+    // if (prop == "id" || prop == "name" || prop == "url") { 
+    //     console.log(prop.toUpperCase() + ":", obj[prop]);
+    //     document.getElementById("id").innerHTML =  obj[prop];
+    //     document.getElementById("name").innerHTML =  obj[prop];
+    //     document.getElementById("url").innerHTML =  obj[prop];
+    // }
